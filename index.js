3b714e459ae0e0922028ff363947d363e725ac39
Tej Bhavsar <bhavsartej97@gmail.com>
2020-07-06 10:52:27 -0400

    hello world query

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import {graphqlHTTP} from 'express-graphql';
+import schema from './schema';
 
 const app = express();
 
 app.get('/', (req,res) => {
     res.send('GraphQL res');
 });
+
+const root = { hello: () => "Hello World!" };
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
 
 app.listen(8080, () => console.log('Running server on Port 8080.'));
